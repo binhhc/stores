d668e3f3ce73bb16c44dfee74be62878d4565ab9
hoangnn <hoangnn@leverages.jp>
2015-01-08 14:43:18 +0700

    Store edit (template, function)

---
+++ app/Controller/StoresController.php
@@ -0,0 +1,184 @@
+<?php
+App::uses('AppController', 'Controller');
+class StoresController extends AppController {
+
+	public $uses = array();
+
+    /**
+     * Displays a store for edit
+     *
+     * @author Nguyen Hoang
+     * @since 2015-01-08
+     *
+     * @return void
+     */
+	public function edit() {
+		$this->layout = 'store_edit';
+	}
+    
+    /**
+     * Load items
+     *
+     * @author Nguyen Hoang
+     * @since 2015-01-08
+     * parameter status
+     * @return json
+     */
+	public function items($status = '') {
+        $this->autoRender = false;
+        $this->response->type('json');
+        
+        $items = array();
+        $item = array(
+            'id' => '54ade2be86b1889a7900144f',
+            'name' => 'Điện máy',
+            'description' => '',
+            'status' => 'shown',
+            'price' => '20000',
+            'sale_flag' => '',
+            'digital_contents' => '',
+            'variations' => array
+            (
+                '0' => 'S',
+                '1' => 'M',
+            ),
+            'quantity' => '2',
+            'shared' => '',
+            'images' => array
+            (
+                '0' => array
+                (
+                    'name' => '76eb0d20782bdab37aa8.jpeg'
+                )
+
+            ),
+            'sticker' => ''
+            
+        );
+        $items[] = $item;
+        
+		if($this->request->is('ajax')){
+            $json = json_encode($items);
+            $this->response->body($json);
+        }
+	}
+    
+    /**
+     * Load user style
+     *
+     * @author Nguyen Hoang
+     * @since 2015-01-08
+     * @return json
+     */
+	public function styles() {
+        $this->autoRender = false;
+        $this->response->type('json');
+        $style = array(
+            'name' => 'hoangnn001',
+            'store_font' => array
+            (
+                'style' => "'Allerta', sans-serif",
+                'type' => 'google',
+                'weight' => '400',
+                'size' => '54',
+            ),
+            'layout' => 'layout_a',
+            'background' => array
+            (
+                'color' => '#fff',
+                'repeat' => '',
+                'image' => '',
+            ),
+            'text_color' => array
+            (
+                'item' => '#000',
+                'store' => '#000'
+            ),
+            'display' => array
+            (
+                'frame' => 1,
+                'item' => 1
+            ),
+            'shipping_fee' => 0,
+            'logo' => ''
+        );
+        
+		if($this->request->is('ajax')){
+            $json = json_encode($style);
+            $this->response->body($json);
+        }
+    
+    }
+    
+    /**
+     * Load user categories
+     *
+     * @author Nguyen Hoang
+     * @since 2015-01-08
+     * @return json
+     */
+	public function categories() {
+        $this->autoRender = false;
+        $this->response->type('json');
+        $categories = array(
+            array(
+                'id' => 1,
+                'name' => 'Điện máy'
+            ),
+            array(
+                'id' => 2,
+                'name' => 'Thời trang'
+            )
+        );
+        
+		if($this->request->is('ajax')){
+            $json = json_encode($categories);
+            $this->response->body($json);
+        }
+    }
+    
+    /**
+     * Load user about
+     *
+     * @author Nguyen Hoang
+     * @since 2015-01-08
+     * @return json
+     */
+	public function about() {
+        $this->autoRender = false;
+        $this->response->type('json');
+        $about = '';
+        
+		if($this->request->is('ajax')){
+            $json = json_encode($about);
+            $this->response->body($json);
+        }
+    }
+    
+     /**
+     * Upload temp image
+     *
+     * @author Nguyen Hoang
+     * @since 2015-01-08
+     * @return json
+     */
+	public function upload_image() {
+        $this->autoRender = false;
+        if(!empty($_FILES['image'])){
+            
+            $file_name = time();
+            $dir = APP . WEBROOT_DIR . DS . '_temp_files' . DS . $file_name.'.jpeg';
+            move_uploaded_file($_FILES['image']['tmp_name'], $dir);
+            $size = getimagesize($dir);
+            
+            $logo = array(
+                'name' => $file_name.'.jpeg',
+                'src' => '/_temp_files/'.$file_name,
+                'ext' => '.jpeg',
+                'org_w' => $size[0],
+                'org_h' => $size[1]
+            );
+            echo json_encode($logo);
+        }
+    }
+}
---
+++ app/View/Layouts/store_edit.ctp
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html ng-app="StoresJp::EditStore">
+<head>
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+  <title>Chỉnh sửa giao diện cửa hàng</title>
+  <?php
+    echo $this->Html->css('application');
+    echo $this->Html->script('edit_store');
+    echo $this->fetch('css');
+    echo $this->fetch('script');
+  ?>
+  <script type="text/javascript">
+    //<![CDATA[
+        AUTH_TOKEN = "123456"; 
+        STORE_ID = '123'; 
+        USER_NAME = 'hoangnn001';
+    //]]>
+</script>
+  <script type="text/javascript">
+    //<![CDATA[
+    STORES_JP = {
+        "FILE_SERVER_URL":"<?php echo Router::url('/', true);?>",
+        //"enable_addons":["follow"]
+    };
+    //]]>
+</script>
+<link href='http://fonts.googleapis.com/css?family=Allerta' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Montserrat:700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Radley' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Marko+One' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Arvo:700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Limelight' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Monoton' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Codystar' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Ribeye+Marrow' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Special+Elite' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Permanent+Marker' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Quicksand:700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Fugaz+One' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Rye' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Vast+Shadow' rel='stylesheet' type='text/css'>
+<style rel="stylesheet" id="dynamic_font_css"></style>
+<style rel="stylesheet" id="select_font_css"></style>
+</head>
+<body id="edit" ng-controller="StylesController" ng-style="styles.body" ng-init="init()">
+<?php echo $this->fetch('content'); ?>
+<script>
+    $(function(){
+      $('.btn_dropdown').on({
+        'mouseenter':function(){
+          $(this).find('.dropdown').fadeIn(100);
+        },
+        'mouseleave':function(){
+          $(this).find('.dropdown').fadeOut(100);
+        }
+      });
+      $('#temp_select').change(function(){
+        var temp = $(this).val();
+        $('#layout_pattern').removeClass().addClass('layout_'+temp);
+      })
+
+      //トグルスイッチ
+      setTimeout(function() {
+        var scope = angular.element('#edit').scope();
+        scope.$watch('styles.logo', function(v) {
+          $('.store_mark .grip').animate({left: (v ? '46px' : '2px')}, 'fast', 'swing');
+        }, true);
+
+        scope.$watch('styles.original_background_image', function(v) {
+          $('#bg_original .grip').animate({left: (v ? '46px' : '2px')}, 'fast', 'swing');
+        }, true);
+
+        scope.$watch('store.display.item', function(v) {
+          $('#item_info .grip :eq(0)').animate({left: (v ? '46px' : '2px')}, 'fast', 'swing');
+        }, true);
+
+        scope.$watch('store.display.frame', function(v) {
+          $('#item_info .grip :eq(1)').animate({left: (v ? '46px' : '2px')}, 'fast', 'swing');
+        }, true);
+      }, 0);
+    });
+  </script>
+  </body>
+</html>
+

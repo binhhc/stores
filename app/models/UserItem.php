84badd69d3e7e3578abd515dd4bf57965ab9b502
Sang PM <sangpm@leverages.jp>
2015-01-14 17:22:52 +0700

    update function getItemSample

--- app/models/UserItem.php
+++ app/models/UserItem.php
@@ -6,12 +6,23 @@
  */
 class UserItem extends Model{
     protected $table  = 'user_items';
-	
+    
+	/**
+     * @author      Sang PM
+     * @since       2015/01/14
+     * 
+     * @modified  
+     * @modified by
+     **/
     public static function getItemSample(){
         $result = array();
         $items = Config::get('constants.item_samples');
         foreach($items as $ite){
-            $result[] = json_decode(json_encode($ite), FALSE);
+            $obj = new UserItem;
+            foreach($ite as $key => $value){
+                $obj->$key = $value;
+            }
+            $result[] = $obj;
         }
         return $result; 
     }

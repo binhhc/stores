c9c83a781bfb7c0bd8b31eddf7906ad6302fba71
Sang PM <sangpm@leverages.jp>
2015-01-14 15:37:36 +0700

    update constants.php

--- app/models/UserItem.php
+++ app/models/UserItem.php
@@ -7,4 +7,12 @@
 class UserItem extends Model{
     protected $table  = 'user_items';
 	
+    public static function getItemSample(){
+        $result = array();
+        $items = Config::get('constants.item_samples');
+        foreach($items as $ite){
+            $result[] = json_decode(json_encode($ite), FALSE);
+        }
+        return $result; 
+    }
 }

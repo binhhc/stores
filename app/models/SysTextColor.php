de36946d11a7281c3d7ab8b99cb520c84fe58181
haule <hauln@leverages.jp>
2015-01-19 08:50:23 +0700

    get system background image, text color, background color

--- app/models/SysTextColor.php
+++ app/models/SysTextColor.php
@@ -6,5 +6,19 @@
  */
 class SysTextColor extends Model{
     protected $table  = 'sys_text_colors';
+
+    /*
+     * @author      Le Nhan Hau
+     * @since       2015/01/16
+     * 
+     * get system text color by
+     */
+    public static function getSysTextColor() {
+        $sysTextColor = DB::table('sys_text_colors')
+            ->where('sys_text_colors.delete_flg', '=', 0)
+            ->orderBy('id', 'desc')
+            ->lists('color');
+        return !empty($sysTextColor) ? $sysTextColor : array();
+    }
 	
 }

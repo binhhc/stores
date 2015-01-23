985f3ecfd0cd9c5d0113511fc6d1d5df9ce5b2f1
binhhc <binhhc@leverages.jp>
2015-01-23 16:41:43 +0700

    update change_profile

--- app/views/user/change_profile.blade.php
+++ app/views/user/change_profile.blade.php
@@ -6,20 +6,27 @@
         <h2 class="heading fl_l">Hồ sơ</h2>
         <p class="fl_r btn_low"><a href="{{URL::asset('/account_setting')}}">Trở lại</a></p>
     </div>
-    <form class="form_basic" name="form">
+    {{Form::open(array('url' => 'change_profile', 'method' => 'post', 'class'=>'form_basic', 'files' => 'true', 'id' => 'frmChangeProfile'))}}
         <dl class="cols">
             <dt>Tên người dùng</dt>
             <dd>
-                {{Form::password('username')}}
-                <font style="color:red;">{{ $errors->first('username') }}</font>
+                {{Form::text('name', isset($user_profile) ? $user_profile->name : null, array('class'=>'txtUsername'))}}
+                <span class="error"></span>
           </dd>
         </dl>
         <dl class="cols">
             <dt>Hình ảnh</dt>
             <dd class="profile_image_fileup">
-                <input class="fileup" type="file" name="profile_image" size="" accept="image/jpeg,image/png,image/gif" onchange="previewFile()">
+                {{Form::file('image_url', array('accept'=>'image/jpeg,image/png,image/gif', 'onchange'=>'previewFile()', 'class'=>'fileup'))}}
                 <p class="profile_image">
-                    {{HTML::image('/img/login/user_icon_01.png', 'Hồ sơ',array('width' => 100, 'height' => '100'))}}
+                    <?php
+                        $url_img = '/img/login/user_icon_01.png';
+                        if(isset($user_profile))
+                            $url_img = $user_profile->image_url;
+                    ?>
+                    {{
+                        HTML::image($url_img, 'Hồ sơ',array('width' => 100, 'height' => '100', 'id'=>'img_loadImagePreview'))
+                    }}
                 </p>
             </dd>
         </dl>
@@ -28,19 +35,47 @@
                 <li class="btn_low">
                     <a href="{{URL::asset('/account_setting')}}">Trở lại</a>
                 </li>
-                <li class="btn_high"><button type="submit">Lưu lại</button></li>
+                <li class="btn_high">
+                    <button type="submit">Lưu lại</button>
+                </li>
             </ul>
             <div class="btn_single btn_low" ng-show="pending" style="display: none;"><button type="submit" class="send"><span>送信中</span></button></div>
         </div>
-    </form>
+    {{Form::close()}}
 </div>
 
 @include('elements.footer')
 
 
 <script type="text/javascript">
+    var submit_flg = true;
+    $(document).ready(function(){
+        $('#frmChangeProfile').submit(function(event){
+            if(submit_flg){
+                var email = $('.email').val();
+                if (validateEmail(email)) {
+                    $('.finish').show();
+                    $('.form_submit').hide();
+                }
+            }else{
+                event.preventDefault();
+            }
+        });
+
+        $('.txtUsername').keyup(function(){
+            var txtUsername = $('.txtUsername').val();
+            $('.error').empty();
+
+            if(txtUsername.length == 0){
+                $('.error').append('Vui lòng nhập tên của bạn');
+                submit_flg = false;
+            }
+        });
+    });
+
+    //load ajax image
     function previewFile(){
-        var preview = document.querySelector('img'); //selects the query named img
+        var preview = document.querySelector('img#img_loadImagePreview'); //selects the query named img
         var file    = document.querySelector('input[type=file]').files[0]; //sames as here
         var reader  = new FileReader();
 
@@ -48,13 +83,18 @@
             preview.src = reader.result;
         }
 
-        if (file) {
-            reader.readAsDataURL(file); //reads the data as a URL
-        } else {
-            preview.src = "";
+        if(!(/\.(gif|jpg|jpeg|tiff|png)$/i).test(file.name)){
+            alert('Không đúng định dạng ảnh!');
+            $('input[name=image_url]').val('');
+            preview.src = "{{$url_img}}";
+            submit_flg = false;
+        }else{
+            if(file){
+                reader.readAsDataURL(file); //reads the data as a URL
+            } else {
+                preview.src = "{{$url_img}}";
+            }
         }
     }
-
-    previewFile();  //calls the function named previewFile()
 </script>
 

35b3fd1e9296e64fc1c692728b7ff9c65a00337d
Sang PM <sangpm@leverages.jp>
2015-02-05 13:31:01 +0700

    add function getData

--- app/models/SysAdver.php
+++ app/models/SysAdver.php
@@ -6,4 +6,15 @@
  */
 class SysAdver extends Model{
     protected $table  = 'sys_advers';
+    
+    /**
+     * @author      Sang PM
+     * @since       2015/02/05
+     * 
+     * @modified  
+     * @modified by
+     **/
+    public static function getData(){
+        return self::get(array('name','href','url'))->toArray();
+    }
 }

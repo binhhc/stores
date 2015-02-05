b7506fc3be6625ed16bb778ab9d539847571f225
oanhht <oanhht@leverages.jp>
2015-02-05 13:08:54 +0700

    - add array constant item slide

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -15,6 +15,10 @@ class MainController extends BaseController {
     public function main(){
     	if(!$this->checkLogin()) {
     		$data['title_for_layout'] = 'Chào mừng đến với stores.vn';
+    		$item_slides = Config::get('constants.item_slide');
+    		$item_more = array('href' => '/category', 'url' => 'img/main_page/top_more.png', 'name' => 'Xem them');
+    		$item_slides[] =  $item_more;
+    		$data['item_slides'] = array_chunk($item_slides, 8);
     		return View::make('main.index', $data);
     	} else {
     		 return Redirect::to('/dashboard');
--- app/views/main/index.blade.php
+++ app/views/main/index.blade.php
@@ -61,174 +61,17 @@
 			</ul>
 			<div class="slide_mask">
 				<div class="slide_wrap">
+				<?php foreach($item_slides as $item) {?>
 					<div class="slide_cols">
 					  <ul class="col4">
+					  <?php foreach ($item as $key => $value) {?>
 						<li class="cols">
-						  <a href="https://anduamet.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/anduamet.jpg', 'anduamet')}}</a>
+						  <a href="{{$value['href']}}" target="_blank">{{HTML::image($value['url'], $value['name'])}}</a>
 						</li>
-						<li class="cols">
-						  <a href="https://ironwork_mk.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/ironwork_mk.jpg', 'ironwork_mk')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="http://shippostore.com/#!/" target="_blank">{{HTML::image('img/main_page/shippostore.jpg', 'ShippoSTORE')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://fleeceleeve.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/fleeceleeve.jpg', 'フリースリーブ')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://chill-garden.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/chill-garden.jpg', 'Chill Garden')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://3355.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/3355.jpg', '3355')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://nakedhorse.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/nakedhorse.jpg', 'nakedhorse')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://tla.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/tla.jpg', 'tla')}}</a>
-						</li>
+						<?php }?>
 					  </ul>
 					</div>
-					<div class="slide_cols pc">
-					  <ul class="col4">
-						<li class="cols">
-						  <a href="https://classico.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/classico.jpg', 'classico')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://patanica.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/patanica.jpg', 'patanica')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://ader.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/ader.jpg', 'ader')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://nohmask.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/nohmask.jpg', 'nohmask')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://butter.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/butter.jpg', 'butter')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://conix.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/conix.jpg', 'conix')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://milkjapon.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/milkjapon.jpg', 'milkjapon')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="http://papierlabo-store.com/#!/" target="_blank">{{HTML::image('img/main_page/papierlabo-store.jpg', 'PAPIER LABO.')}}</a>
-						</li>
-					  </ul>
-					</div>
-					<div class="slide_cols pc">
-					  <ul class="col4">
-						<li class="cols">
-						  <a href="https://nstore.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/nstore.jpg', 'N STORE')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://molansoda.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/molansoda.jpg', 'molansoda')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://1012.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/1012.jpg', '1012')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://shoesofprey.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/shoesofprey.jpg', 'shoesofprey')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://leather_labo.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/leather_labo.jpg', 'leather_labo')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://ciito.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/ciito.jpg', 'ciito')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://kotoriten.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/kotoriten.jpg', 'kotoriten')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://organics.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/organics.jpg', 'organics')}}</a>
-						</li>
-					  </ul>
-					</div>
-					<div class="slide_cols pc">
-					  <ul class="col4">
-						<li class="cols">
-						  <a href="https://drawingandmanual.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/drawingandmanual.jpg', 'drawingandmanual')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://feacoffee.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/feacoffee.jpg', 'feacoffee')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://no8vani.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/no8vani.jpg', 'no8vani')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://bowtie.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/bowtie.jpg', 'bowtie')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://handmadetights.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/handmadetights.jpg', 'handmadetights')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://neon.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/neon.jpg', 'neon')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://actroom.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/actroom.jpg', 'actroom')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://btfurniture.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/btfurniture.jpg', 'btfurniture')}}</a>
-						</li>
-					  </ul>
-					</div>
-					<div class="slide_cols pc">
-					  <ul class="col4">
-						<li class="cols">
-						  <a href="https://maru-bizen.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/maru-bizen.jpg', 'maru-bizen style')}}</a>
-						</li>
-						<li class="cols">
-					  <a href="https://kanvas.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/kanvas.jpg', 'kanvas')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://1740avenue.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/1740avenue.jpg', '1740 AVENUE')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://quaela.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/quaela.jpg', 'qua e la')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://specialfresh.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/specialfresh.jpg', 'SpecialFRESH')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://shiroikuro.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/shiroikuro.jpg', 'Shiroi Kuro')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://yortz.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/yortz.jpg', 'YORTZ')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://amani.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/amani.jpg', 'amani')}}</a>
-						</li>
-					  </ul>
-					</div>
-					<div class="slide_cols pc">
-					  <ul class="col4">
-						<li class="cols">
-						  <a href="https://dolcedeco.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/dolcedeco.jpg', 'Dolce Deco')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://quaintdesign.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/quaintdesign.jpg', 'Quaint Design')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://dls.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/dls.jpg', 'dls')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://stereotennis.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/stereotennis.jpg', 'STEREO TENNIS FANCY SHOP')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://arcobaleno.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/arcobaleno.jpg', 'arcobaleno')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://sowxp.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/sowxp.jpg', 'sowxp')}}</a>
-						</li>
-						<li class="cols">
-						  <a href="https://doramik.stores.jp/#!/" target="_blank">{{HTML::image('img/main_page/doramik.jpg', 'DORAMIK')}}</a>
-						</li>
-						<li class="cols more">
-						  <a href="/category">{{HTML::image('img/main_page/top_more.png', 'もっと見る')}}<p>Tiếp tục</p></a>
-						</li>
-					  </ul>
-					</div>
+					<?php }?>
 			    </div>
 			    <p class="sp_category_more">
 					<a href="">Phân loại</a>

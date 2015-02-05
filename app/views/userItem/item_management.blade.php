72d523c41764bfdc290810c958729078afa31e53
oanhht <oanhht@leverages.jp>
2015-02-05 11:24:17 +0700

    - add facebook share and tweet for item management

--- app/views/userItem/item_management.blade.php
+++ app/views/userItem/item_management.blade.php
@@ -1,4 +1,5 @@
 @include('elements.header')
+<script src="//connect.facebook.net/ja_JP/all.js"></script>
  {{HTML::script('/js/item_management.js')}}
 <div id="alert_panel" class="success" style="display: none; opacity: 1.0; top: -10px;">
     <p>Đã xoá thành công!</p>
@@ -78,7 +79,15 @@
                                         <a href="{{URL::asset('/edit_item/'.Crypt::encrypt($item['id']))}}">Sửa</a>
                                     </li>
                                     <li class="navi_share" item_id="{{$item['id']}}">
-                                        <p class="navi_share_btn" ng-class="item_share(item)">Xem</p>
+                                        <p class="navi_share_btn_true" ng-class="item_share(item)">Xem</p>
+                                        <ul class="baloon1 navi_share_baloon tooltip_share{{$item['id']}}" ng-class="{navi_share_baloon: is_postable_parco_blog(item) == false && is_exblog == false && is_postable_parcocity_blog(item) == false}" data-share-display="false" style="display: none;">
+											<li class="navi_share_fb">
+												<div class="fb-share-button" data-layout="icon_link"><a ng-click="postToFeedItem(item)" class="share_facebook" href="" item_name="{{$item['name']}}" image_url="{{$item['image_url']}}">Chia sẻ</a></div>
+											</li>
+											<li class="navi_share_tw">
+												<a ng-click="share_sns(item)" href="http://twitter.com/intent/tweet?ount=none&lang='vi'&text={{$item['name']}} / https%3A%2F%2Foanhht.stores.jp%2F%23!%2Fitems%2F54bf2b9986b188c7540009f9 @stores_jp" target="_blank">Tweet</a>
+											</li>
+										</ul>
                                     </li>
                                 </ul>
                             </dd>
@@ -92,6 +101,7 @@
 </div>
 <div class="loading ng-scope" ng-show="state == 'wait'" style="display: none;"></div>
 @include('elements.footer')
+<input type="hidden" name="facebook_id" class="facebook_app_id" value="{{$app_id}}"/>
 <style>
  .switch .deactive {
     text-indent: 1em!important;
@@ -105,3 +115,14 @@
 }
 </style>
 
+<div id="fb-root"></div>
+<script>
+var app_id = $('.facebook_app_id').val();
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : app_id,
+      xfbml      : true,
+      version    : 'v2.2'
+    });
+  };
+</script>

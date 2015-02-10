c758f3448b8f02bbbbd8cb65ba0bb2540d03c6b8
oanhht <oanhht@leverages.jp>
2015-02-10 11:35:40 +0700

    - add link

--- app/views/elements/footer.blade.php
+++ app/views/elements/footer.blade.php
@@ -3,10 +3,10 @@
             <p class="support_banner"><a  href="{{URL::asset('/support')}}">Hạn chế hỗ trợ hoạt động</a></p>
             <p class="referral_banner"><a  href="{{URL::asset('/referral')}}">Giới thiệu chiến dịch khuyến mãi</a></p>
             <ul id="foot_navi">
-                <li><a href="http://storesjpinfo.tumblr.com/" target="_blank">Thông báo</a></li>
+                <li><a href="/notification" target="_blank">Thông báo</a></li>
                 <li><a href="/contact" target="_blank">Liên hệ</a></li>
                 <li><a href="/terms" target="_blank">Điều khoản sử dụng</a></li>
-                <li><a href="/tokushoho" target="_blank">Luật thương mại</a></li>
+                <li><a href="/law" target="_blank">Luật thương mại</a></li>
                 <li><a href="/privacy_policy" target="_blank">Chính sách bảo mật</a></li>
                 <li><a href="http://leverages.vn/" target="_blank">Công ty vận hành</a></li>
                 <li class="logout">

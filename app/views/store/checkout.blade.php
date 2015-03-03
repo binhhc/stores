edd50fb5b585e56c040d514aac99e20591e6ad1e
Sang PM <sangpm@leverages.jp>
2015-03-03 11:01:22 +0700

    update currency

--- app/views/store/checkout.blade.php
+++ app/views/store/checkout.blade.php
@@ -43,33 +43,33 @@
               <dd class="sz_i"><img ng-src="{[cart_item.image.src]}"></dd>
               <dd class="sz_l">{[cart_item.name]}</dd>
               <dd class="sz_s tl">{[cart_item.variation]}</dd>
-              <dd class="sz_s tr">Â¥{[cart_item.price | number:0]}</dd>
+              <dd class="sz_s tr"><?php echo Config::get('constants.item.currency'); ?>{[cart_item.price | number:0]}</dd>
               <dd class="sz_s tc">{[cart_item.quantity]}</dd>
-              <dd class="sz_s tr">Â¥{[(cart_item.price * cart_item.quantity) | number:0]}</dd>
+              <dd class="sz_s tr"><?php echo Config::get('constants.item.currency'); ?>{[(cart_item.price * cart_item.quantity) | number:0]}</dd>
             </dl>
           </dd>
           <dd class="lists shipping checkout">
             <dl>
               <dd class="sz_ex tr">{[I18n.store.cart.shippingFee]}</dd>
-              <dd class="sz_s tr">Â¥{[cart.shipping_fee | number:0]}</dd>
+              <dd class="sz_s tr"><?php echo Config::get('constants.item.currency'); ?>{[cart.shipping_fee | number:0]}</dd>
             </dl>
           </dd>
           <dd class="lists shipping checkout" ng-show="customer.payment_method && customer.payment_method[2] > 0">
             <dl>
               <dd class="sz_ex tr">{[customer.payment_method[1]]}æ‰‹æ•°æ–™</dd>
-              <dd class="sz_s tr">Â¥{[customer.payment_method[2] | number:0]}</dd>
+              <dd class="sz_s tr"><?php echo Config::get('constants.item.currency'); ?>{[customer.payment_method[2] | number:0]}</dd>
             </dl>
           </dd>
           <dd class="lists shipping checkout" style="display:none;" ng-show="coupon">
             <dl>
                 <dd class="sz_s tr">{[I18n.store.checkout.coupon]}</dd>
-              <dd class="sz_s tr" style="color: red;">- Â¥{[coupon.price | number:0]}</dd>
+              <dd class="sz_s tr" style="color: red;">- <?php echo Config::get('constants.item.currency'); ?>{[coupon.price | number:0]}</dd>
             </dl>
           </dd>
           <dd class="lists total checkout">
             <dl>
                 <dd class="sz_ex tr">{[I18n.store.cart.total]}</dd>
-              <dd class="sz_s tr">Â¥{[cart.total | number:0]}</dd>
+              <dd class="sz_s tr"><?php echo Config::get('constants.item.currency'); ?>{[cart.total | number:0]}</dd>
             </dl>
           </dd>
           <dd class="coupon" ng-hide="(coupon || !enable_coupon) || cart.items[0].mybook_item">

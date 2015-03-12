2c22f06cc8c429a8ae92bc777e5d1692d3df0f0a
haule <hauln@leverages.jp>
2015-03-12 09:54:21 +0700

    fix bug model name

--- app/models/UserItemQuantity.php
+++ app/models/UserItemQuantity.php
@@ -5,7 +5,7 @@
  * @author sangpm
  */
 class UserItemQuantity extends Model{
-    protected $table  = 'user_item_quantities';
+    protected $table  = 'user_items_quantities';
 
     /**
      * Defines an inverse one-to-many relationship.

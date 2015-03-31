7771039226172c5408a8bbd04cfe4e131cd293ed
haule <hauln@leverages.jp>
2015-03-31 13:48:50 +0700

    fix layout login page

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -9,20 +9,20 @@
         <div class="sign_up">
             <h1>
                 <a href="#">
-                    {{HTML::image('img/login/logo_white.png', 'STORES.vn')}}
+                    {{HTML::image('img/login/logo_hayamise_login.png', 'STORES.vn')}}
                 </a>
             </h1>
+            <div class="login_separator">{{HTML::image('img/login/colorful-separator.png', 'STORES.vn')}}</div>
             <div class="box">
                 {{Form::open(array('url' => 'login', 'method' => 'post'))}}
+                    <input type="password" style="display:none">
                     <dl class="set">
-                        <dt>Địa chỉ email</dt>
                         <dd>
-                            {{Form::text('email', null, array('class' => 'input_login'))}}
+                            {{Form::text('email', null, array('class' => 'input_login', 'placeholder'=>'Tên đăng nhập', 'autocomplete'=>'off'))}}
                             <font class="message_error">{{ $errors->first('email') }}</font>
                         </dd>
-                        <dt>Mật khẩu</dt>
                         <dd>
-                            {{Form::password('password', array('class' => 'input_login'))}}
+                            {{Form::password('password', array('class' => 'input_login', 'placeholder'=>'Mật khẩu', 'autocomplete'=>'off'))}}
                             <font class="message_error">{{ $errors->first('password') }}</font>
                         </dd>
                     </dl>
@@ -30,40 +30,42 @@
                      <input type="hidden" name="item_id" value="<?php echo isset($item_id) ? $item_id : '' ?>"/>
 					<input type="hidden" name="store_user_id" value="<?php echo isset($store_user_id) ? $store_user_id : '' ?>"/>
                     <p class="btn_submit inner_s top">
-                        <button type="submit" style="padding-top:0px">Đăng nhập</button>
+                        <button type="submit" style="padding-top:0px">ĐĂNG NHẬP</button>
                     </p>
 
                     @if($message = Session::get('message'))
                         <div class="message_login">{{ $message }}</div>
                     @endif
 
+                    <div class="social_login_or">
+                        <div class="hight_line"></div>
+                        <div class="or_text">hoặc</div>
+                    </div>
+                    
                     <div class="social_login">
                         <p class="btn_facebook">
                         <?php if(isset($store_user_id) && isset($redirect_url)) {?>
                         	 <a href="/login/fb?store_user_id=<?php echo md5($store_user_id)?>&redirect_url=<?php echo $redirect_url?>">
-                                Facebook
+                                ĐĂNG NHẬP FACEBOOK
                             </a>
                         <?php } else { if(isset($item_id) && isset($redirect_url)) {?>
 								<a href="/login/fb?item_id=<?php echo $item_id?>&redirect_url=<?php echo $redirect_url?>">
-                                Facebook
+                                ĐĂNG NHẬP FACEBOOK
                             </a>
 							<?php } else {?>
                             <a href="{{url('login/fb')}}">
-                                Facebook
+                                ĐĂNG NHẬP FACEBOOK
                             </a>
                         <?php } }?>
                         </p>
                     </div>
                 {{ Form::close() }}
+                
+                <div class="login_link">
+                    <div class="login_link_back_home"><a href="{{URL::asset('/')}}">Quay lại trang chủ</a></div>
+                    <div class="login_link_forgot_password">{{HTML::link('/forgetPassword', 'Quên mật khẩu?', array('class' => ''))}}</div>
+                </div>
             </div>
-            <ul class="link">
-                <li>
-                    <a href="{{URL::asset('/')}}">Quay lại trang chủ</a>
-                </li>
-                <li>
-                    {{HTML::link('/forgetPassword', 'Quên mật khẩu', array('class' => ''))}}
-                </li>
-            </ul>
         </div>
     </body>
 </html>

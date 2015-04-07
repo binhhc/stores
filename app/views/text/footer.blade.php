324479d89707c36f2d7670153bdfda4f77a968b5
haule <hauln@leverages.jp>
2015-04-07 13:05:59 +0700

    fix layout

--- app/views/text/footer.blade.php
+++ app/views/text/footer.blade.php
@@ -1,3 +1,15 @@
+        <div id="footer">
+            <div class="foot_navi_wrap">
+                <ul>
+                    <li><a href="/contact" target="">LIÊN HỆ</a></li><span> | </span>
+                    <li><a href="/terms" target="">ĐIỀU KHOẢN SỬ DỤNG</a></li><span> | </span>
+                    <li><a href="/law" target="">LUẬT THƯƠNG MẠI</a></li><span> | </span>
+                    <li><a href="/privacy_policy" target="">CHÍNH SÁCH BẢO MẬT</a></li><span> | </span>
+                    <li><a href="http://leverages.vn/" target="">CÔNG TY VẬN HÀNH</a></li>    
+                </ul>
+            </div>
+        </div>
+        <?php /*?>
          <!-- Footer/ -->
         <div id="footer" class="row">
             <ul id="foot_navi">
@@ -12,5 +24,6 @@
 			</ul>
         </div>
         <!-- /Footer -->
+        <?php */?>
     </body>
 </html>
